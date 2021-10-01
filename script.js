54e167e5264ebfb962f93dfd33095354ffb2f490
MadSamson <m.mahdi.samadi@gmail.com>
2021-10-01 10:23:20 +0200

    version 1-0

---
+++ script.js
@@ -0,0 +1,79 @@
+const coffees = [
+    {name: 'Brygg Kaffe',price: 20},
+    {name: 'Cappucino',price: 30},
+    {name: 'Latte',price: 40}
+]
+let totalNumberOfCups = 0
+let costs = 0
+let totalCost = 0
+let membersStatus = ""
+let remaining = 0
+let _10PercentDiscount = 0
+let _15PercentDiscount = 0
+let finalCost = 0
+let myArchive = []
+
+function master() { 
+    const numberOfCups = Number(document.getElementById("takeOrders").value)
+    // input validation
+    if (numberOfCups >= 1 && numberOfCups <= 10) {
+        // cost calculation
+        totalNumberOfCups = totalNumberOfCups + numberOfCups
+        const coffeeIndex = document.getElementById("coffeeTypes").selectedIndex
+        costs = numberOfCups * coffees[coffeeIndex].price
+        totalCost += costs
+        finalCost = totalCost
+        // giving discount
+        if (totalCost>=500) {
+            _10PercentDiscount = ((totalCost-500)*0.9)+500
+            finalCost = _10PercentDiscount
+            console.log(_10PercentDiscount);
+            if (_10PercentDiscount>=1000) {
+                _15PercentDiscount = ((_10PercentDiscount-1000)*0.85)+1000
+                finalCost = _15PercentDiscount
+                console.log(_15PercentDiscount);
+            }
+        }
+
+        // getting members status
+        if (totalNumberOfCups < 10) {
+            membersStatus = "Brons"
+        } else if (totalNumberOfCups >= 10 && totalNumberOfCups < 30) {
+            membersStatus = "Silver"
+        } else if (totalNumberOfCups >= 30) {
+            membersStatus = "Guld"
+        }
+        const parent = document.getElementById("parent").innerHTML = `Du har handlat för ${finalCost}kr <br> MedlemsStatus: ${membersStatus}`
+
+        // transation & History 
+        const changeTitle = document.getElementById("title")
+        changeTitle.innerHTML = "Dina Transaktioner"
+        const theOtherParent = document.getElementById("second")
+        const p = document.createElement("p")
+        p.innerHTML = `Du har köpt ${numberOfCups}st ${coffees[coffeeIndex].name} för ${coffees[coffeeIndex].price}kr styck. Summa: ${numberOfCups*coffees[coffeeIndex].price}`
+        theOtherParent.appendChild(p)
+        const putInFirstLn = document.getElementById("letItBeFirst")
+        putInFirstLn.insertBefore(p, putInFirstLn.childNodes[0])
+        
+        // archiving everything
+        class Archiveclass{
+            constructor(FinalCost, CoffeType, NumberOfCups) {
+                this.FinalCost = FinalCost
+                this.CoffeType = CoffeType
+                this. NumberOfCups = NumberOfCups
+            }
+        }
+        let archiveObj = new Archiveclass(finalCost, coffees[coffeeIndex].name, numberOfCups)
+        myArchive.push(archiveObj)
+        console.log(myArchive);
+    } else if (numberOfCups>10) {
+        alert("we don't take order more than ten cups")
+    } else {
+        alert ("invalid value")
+    }
+}
+
+// emptying the text field
+function emptyTextField() {
+    document.getElementById("takeOrders").value = ""
+}
