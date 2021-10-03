c5409dd5e9f8955b19ecd1aa6b819f4639aaae16
MadSamson <m.mahdi.samadi@gmail.com>
2021-10-03 20:31:23 +0200

    version 1-2

--- script.js
+++ script.js
@@ -3,76 +3,88 @@ const coffees = [
     {name: 'Cappucino',price: 30},
     {name: 'Latte',price: 40}
 ]
+let numberOfCups = 0
 let totalNumberOfCups = 0
-let costs = 0
-let totalCost = 0
+let prices = 0
+let totalPrice = 0
+let finalPrice = 0
 let membersStatus = ""
 let remaining = 0
-let With10PercentOff = 0
-let With15PercentOff = 0
-let finalCost = 0
+let coffeeIndex = null
+let discountAmount = 0
 let myArchive = []
 
-function master() { 
-    const numberOfCups = Number(document.getElementById("takeOrders").value)
-    // input validation
+function master() {
+    getInput()
+}
+// input validation
+function getInput() {
+    numberOfCups = Number(document.getElementById("takeOrders").value)
     if (numberOfCups >= 1 && numberOfCups <= 10) {
-        // cost calculation
-        totalNumberOfCups = totalNumberOfCups + numberOfCups
-        const coffeeIndex = document.getElementById("coffeeTypes").selectedIndex
-        costs = numberOfCups * coffees[coffeeIndex].price
-        totalCost += costs
-        finalCost = totalCost
-        // giving discount
-        if (totalCost>=500) {
-            With10PercentOff = ((totalCost-500)*0.9)+500
-            finalCost = With10PercentOff
-            console.log(With10PercentOff);
-            if (With10PercentOff>=1000) {
-                With15PercentOff = ((With10PercentOff-1000)*0.85)+1000
-                finalCost = With15PercentOff
-                console.log(With15PercentOff);
-            }
-        }
-
-        // getting members status
-        if (totalNumberOfCups < 10) {
-            membersStatus = "Brons"
-        } else if (totalNumberOfCups >= 10 && totalNumberOfCups < 30) {
-            membersStatus = "Silver"
-        } else if (totalNumberOfCups >= 30) {
-            membersStatus = "Guld"
-        }
-        const parent = document.getElementById("parent").innerHTML = `Du har handlat för ${finalCost}kr <br> MedlemsStatus: ${membersStatus}`
-
-        // transation & History 
-        const changeTitle = document.getElementById("title")
-        changeTitle.innerHTML = "Dina Transaktioner"
-        const theOtherParent = document.getElementById("second")
-        const p = document.createElement("p")
-        p.innerHTML = `Du har köpt ${numberOfCups}st ${coffees[coffeeIndex].name} för ${coffees[coffeeIndex].price}kr styck. Summa: ${numberOfCups*coffees[coffeeIndex].price}`
-        theOtherParent.appendChild(p)
-        const putInFirstLn = document.getElementById("letItBeFirst")
-        putInFirstLn.insertBefore(p, putInFirstLn.childNodes[0])
-        
-        // archiving everything
-        class Archiveclass{
-            constructor(FinalCost, CoffeType, NumberOfCups) {
-                this.FinalCost = FinalCost
-                this.CoffeType = CoffeType
-                this. NumberOfCups = NumberOfCups
-            }
-        }
-        let archiveObj = new Archiveclass(finalCost, coffees[coffeeIndex].name, numberOfCups)
-        myArchive.push(archiveObj)
-        console.log(myArchive);
-    } else if (numberOfCups>10) {
-        alert("we don't take order more than ten cups")
+        getCost()
+        discount()
+        getStatus()
+        transactionHistory()
+        archiving()
+    } else if (numberOfCups > 10) {
+        alert("you can only order up to ten")
     } else {
-        alert ("invalid value")
+        alert("invalid value")
     }
 }
-
+// giving discount
+function discount() {
+    if (totalPrice > 1000) {
+        remaining = totalPrice - 1000
+        discountAmount = (500 * 0.1) + (remaining * 0.15)
+    } else if (totalPrice > 500) {
+        remaining = totalPrice - 500
+        discountAmount = remaining * 0.1
+    }
+    finalPrice = totalPrice - discountAmount
+}
+// cost calculation
+function getCost() {
+    totalNumberOfCups = totalNumberOfCups + numberOfCups
+    coffeeIndex = document.getElementById("coffeeTypes").selectedIndex
+    prices = numberOfCups * coffees[coffeeIndex].price
+    totalPrice = totalPrice + prices
+}
+// getting members status
+function getStatus() {
+    if (totalNumberOfCups < 10) {
+        membersStatus = "Brons"
+    } else if (totalNumberOfCups >= 10 && totalNumberOfCups < 30) {
+        membersStatus = "Silver"
+    } else if (totalNumberOfCups >= 30) {
+        membersStatus = "Guld"
+    }
+    const parent = document.getElementById("parent").innerHTML = `Du har handlat för ${finalPrice}kr <br> MedlemsStatus: ${membersStatus}`
+}
+// transation & History
+function transactionHistory() {
+    const changeTitle = document.getElementById("title")
+    changeTitle.innerHTML = "Dina Transaktioner"
+    const theOtherParent = document.getElementById("second")
+    const p = document.createElement("p")
+    p.innerHTML = `Du har köpt ${numberOfCups}st ${coffees[coffeeIndex].name} för ${coffees[coffeeIndex].price}kr styck. Summa: ${numberOfCups*coffees[coffeeIndex].price}`
+    theOtherParent.appendChild(p)
+    const putInFirstLn = document.getElementById("letItBeFirst")
+    putInFirstLn.insertBefore(p, putInFirstLn.childNodes[0])
+}
+// archiving everything
+function archiving() {
+    class Archiveclass {
+        constructor(FinalCost, CoffeType, NumberOfCups) {
+            this.FinalCost = FinalCost
+            this.CoffeType = CoffeType
+            this.NumberOfCups = NumberOfCups
+        }
+    }
+    let archiveObj = new Archiveclass(finalPrice, coffees[coffeeIndex].name, numberOfCups)
+    myArchive.push(archiveObj)
+    console.log(myArchive);
+}
 // emptying the text field
 function emptyTextField() {
     document.getElementById("takeOrders").value = ""
